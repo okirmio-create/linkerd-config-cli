f4ea60d5385ca28346d0b796804b11687d521921
lermi <lermi@users.noreply.github.com>
2026-04-06 16:59:09 +0300

    feat: initial release of linkerd-config-cli v1.0.0

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,8 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
-  target: "node18",
   clean: true,
-  minify: false,
+  dts: true,
   banner: {
     js: "#!/usr/bin/env node",
   },
